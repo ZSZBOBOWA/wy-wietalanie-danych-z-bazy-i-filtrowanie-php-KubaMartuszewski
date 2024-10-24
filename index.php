4b24af6af45e65f596adc58665e3ed547d496862
Jakub <jakub.martuszewski@zsz.bobowa.pl>
2024-10-24 08:56:39 +0200

    projekt

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
+<form method="POST" action="index.php">
+    Wpisz nazwisko: <input type="text" name="nazwisko">
+    <input type="submit" value="Filtruj">
+</form>
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "szkola";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-
 if (!$conn) {
     die("Błąd połączenia: " . mysqli_connect_error());
 }
-?>
-<?php
-$sql = "SELECT * FROM uczniowie";
+
+if(isset($_POST['nazwisko']) && $_POST['nazwisko'] != '') {
+    $nazwisko = $_POST['nazwisko'];
+    $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
+
+    $sql = "SELECT * FROM uczniowie WHERE nazwisko='$nazwisko'";
+} else {
+    $sql = "SELECT * FROM uczniowie";
+}
+
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -23,5 +32,5 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     echo "Brak wyników";
 }
-mysqli_close($conn):
+mysqli_close($conn);
 ?>
